1c9f13f4aba5693adaf2d75af403e51b68901e4a
nthomson <nthomsondev@gmail.com>
2011-06-23 13:31:36 -0400

    Re-organizing the way cloudfiles are hosted

--- cloud_handlers.py
+++ cloud_handlers.py
@@ -1,15 +1,16 @@
+from django.conf import settings
 import cloudfiles
 
 def upload_cloud_file( fd, user, project, page, revision ):
-    cloud_user = 'RACKSPACE_USER'
-    cloud_key = 'RACKSPACE_KEY'
+    cloud_user = settings.CLOUD_USER
+    cloud_key = settings.CLOUD_KEY
     cloud_container = 'images'
     
     connection = cloudfiles.get_connection(cloud_user, cloud_key)
-    container = connection.get_container(cloud_container)
-    
+    container = connection.create_container('%s_%s' % (user, project))
+    container.make_public(ttl=604800)
     #open a file and write the descriptor
-    ob = container.create_object('%s_%s_%s_%s_%s' % (user, project, page, revision, fd.name) )
+    ob = container.create_object('%s_%s_%s' % (page, revision, fd.name) )
     ob.content_type = fd.content_type
     for chunk in fd.chunks():
         ob.write(chunk)
